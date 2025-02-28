7e911b4076afb1b49e61b00fb59af785b3d9f0f9
Solomon B. Pobee <jnr.pobee@gmail.com>
2025-02-27 17:12:48 -0700

    working on the server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,20 +1,33 @@
 package server;
 
+import dataaccess.service.ClearService;
+import dataaccess.service.JoinGameService;
+import dataaccess.service.ListGameService;
+import dataaccess.service.LoginService;
+import dataaccess.service.LogoutService;
+import dataaccess.service.RegisterService;
 import spark.*;
 
+
 public class Server {
+    private RegisterService registerService;
+    private LoginService loginService;
+    private LogoutService logoutService;
+    private ListGameService listGameService;
+    private JoinGameService joinGameService;
+    private ClearService clearService;
+
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
-
-        // Register your endpoints and handle exceptions here.
-
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
+        
+
         return Spark.port();
     }
 
@@ -26,4 +39,6 @@ public class Server {
     public int port() {
         return Spark.port();
     }
+
+    
 }
