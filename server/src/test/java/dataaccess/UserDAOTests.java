61400e90771838b181c58712cacbb40215ececdc
Solomon B. Pobee <jnr.pobee@gmail.com>
2025-03-12 21:54:48 -0600

    working on userDAO Test

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -1,4 +1,79 @@
 package dataaccess;
 
+import dataaccess.mysql.MySQLUserDAO;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class UserDAOTests {
+    private static final UserDAO userDAO;
+
+    static {
+        try {
+            userDAO = new MySQLUserDAO();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Test
+    void positiveClear() {
+        Assertions.assertDoesNotThrow(userDAO::clear);
+        try {
+            Assertions.assertFalse(userDAO.isUser(new UserData(
+                    "name",
+                    "password",
+                    "email@email.com"
+            )));
+        } catch (DataAccessException e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    void positiveIsUser() {
+        try {
+            userDAO.createUser(new UserData(
+                    "name",
+                    "password",
+                    "email@email.com"
+            ));
+        } catch (DataAccessException ignored) {
+        }
+        Assertions.assertDoesNotThrow(() -> userDAO.isUser(new UserData(
+                "name",
+                "password",
+                "email@email.com"
+        )));
+        try {
+            Assertions.assertTrue(userDAO.isUser(new UserData(
+                    "name",
+                    "password",
+                    "email@email.com"
+            )));
+        } catch (DataAccessException e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    void negativeIsUser() {
+        Assertions.assertDoesNotThrow(() -> userDAO.isUser(new UserData(
+                "wrongName",
+                "wrongPassword",
+                "wrongEmail@email.com"
+        )));
+        try {
+            Assertions.assertFalse(userDAO.isUser(new UserData(
+                    "badName",
+                    "badPass",
+                    "badEmail@email.com"
+            )));
+        } catch (DataAccessException e) {
+            Assertions.fail();
+        }
+    }
+
+
 }
