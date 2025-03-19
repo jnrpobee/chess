23886299b3741650f9a0ac0586d60fe1be206d3d
Solomon B. Pobee <jnr.pobee@gmail.com>
2025-03-19 16:52:23 -0600

    class serverfacade created

---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,78 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerFacade {
+    private final String serverUrl;
+    private String authToken;
+    
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws DataAccessException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (DataAccessException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, DataAccessException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+//            try (InputStream respErr = http.getErrorStream()) {
+//                if (respErr != null) {
+//                    throw DataAccessException.fromJson(respErr);
+//                }
+//            }
+
+            throw new DataAccessException("other failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
