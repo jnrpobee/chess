a22e82eda4637402aa9313feb80d4db3d1565303
Solomon B. Pobee <jnr.pobee@gmail.com>
2025-04-06 23:25:17 -0600

    done with commands

--- shared/src/main/java/websocket/commands/ConnectCommand.java
+++ shared/src/main/java/websocket/commands/ConnectCommand.java
@@ -1,4 +1,26 @@
 package websocket.commands;
 
-public class ConnectCommand {
+import chess.ChessGame;
+
+public class ConnectCommand extends UserGameCommand {
+    private final ChessGame.TeamColor playerColor;
+
+    public ConnectCommand(String authData, int gameID, ChessGame.TeamColor playerColor) {
+        super(CommandType.CONNECT, authData, gameID);
+        this.playerColor = playerColor;
+    }
+
+    public ChessGame.TeamColor getPlayerColor() {
+        return playerColor;
+    }
+
+    // @Override
+    // public String toString() {
+    //     return "ConnectCommand{" +
+    //             "authData='" + authData + '\'' +
+    //             ", gameID=" + gameID +
+    //             ", playerColor=" + playerColor +
+    //             ", commandType=" + commandType +
+    //             '}';
+    // }
 }
--- shared/src/main/java/websocket/commands/LeaveCommand.java
+++ shared/src/main/java/websocket/commands/LeaveCommand.java
@@ -1,4 +1,17 @@
 package websocket.commands;
 
-public class LeaveCommand {
+public class LeaveCommand extends UserGameCommand {
+
+    public LeaveCommand(String authData, int gameID) {
+        super(CommandType.LEAVE, authData, gameID);
+    }
+
+    // @Override
+    // public String toString() {
+    //     return "LeaveCommand{" +
+    //             "authData='" + authData + '\'' +
+    //             ", gameID=" + gameID +
+    //             ", commandType=" + commandType +
+    //             '}';
+    // }
 }
